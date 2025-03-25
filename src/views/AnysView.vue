579b3e3ae5a8c674819d7393cf6703c7e257b528
= <=>
2025-03-25 17:13:59 +0100

    upload files

---
+++ src/views/AnysView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios
+
+const data = ref([])
+const anys = ref([])
+</script>
+<template>
+  <h1>AnysView</h1>
+</template>
+<style scoped></style>
